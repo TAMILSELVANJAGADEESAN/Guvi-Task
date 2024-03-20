e667e8ea9dec75aa5671f248cb0d376a3b0bd458
TAMILSELVAN <142396484+TAMILSELVANJAGADEESAN@users.noreply.github.com>
2024-03-20 18:38:39 +0530

    update.php

--- php/update.php
+++ php/update.php
@@ -1,29 +1,50 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+require 'vendor/autoload.php'; 
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$mongoClient = new MongoDB\Client("mongodb://localhost:27017");
+$database_name = $mongoClient->selectDatabase('registration');
+$collection_name = $database_name->selectCollection('profiledetails');
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    $name = $_POST['name'];
+    $gender = $_POST['gender'];
+    $dob =$_POST['dob'];
+    $phone = $_POST['phone'];
+
+    
+    $insertResult = $collection_name->insertOne([
+        'name' => $name,
+        'gender' => $gender,
+        'dob' => $dob,
+        'phone' => $phone
+    ]);
+
+    if ($insertResult->getInsertedCount() > 0) {
+        $cursor = $collection_name->find();
+
+        
+        echo '<div class="container">';
+        echo '<div id="profile-container">';
+        echo '<h4>Profile Details</h4>';
+
+        
+            echo '<div class="profile-details">';
+            echo '<p><strong>Name:</strong> '. $name .'</p>';
+            echo '<p><strong>Gender:</strong>'. $gender.'</p>';
+            echo '<p><strong>Birthday:</strong>'. $dob.'</p>';
+            echo '<p><strong>Phone Number:</strong> ' . $phone. '</p>';
+            echo '</div>';
+        
+
+        echo '</div>';
+        echo '</div>';
+    }
+    else{
+        echo 'Error in inserting the data';
+    }
+
 }
-
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-$sql = "UPDATE register SET fname='$fname', lname='$lname', password='$password' WHERE email='$email'";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Record updated successfully";
-} else {
-    echo "Error updating record: " . $conn->error;
-}
-
-$conn->close();
 ?>
